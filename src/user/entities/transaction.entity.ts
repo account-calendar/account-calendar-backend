06ac7ab1e9b0b18d047cef4a3100748ad39c140f
hjpark625 <zero950@gmail.com>
2025-06-13 18:19:33 +0900

    feat: 수입지출 내역 엔티티 정의

---
+++ src/user/entities/transaction.entity.ts
@@ -0,0 +1,53 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+import { User } from '@/user/entities/user.entity'
+import { MajorCategory } from '@/user/entities/major-category.entity'
+import { MiddleCategory } from '@/user/entities/middle-category.entity'
+
+@Entity('transaction')
+export class Transaction {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number
+
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn({ name: ' user-id' })
+  user: User
+
+  @ManyToOne(() => MajorCategory, { nullable: true })
+  @JoinColumn({ name: 'major_category_id' })
+  majorCategory: MajorCategory | null
+
+  @ManyToOne(() => MiddleCategory, { nullable: true })
+  @JoinColumn({ name: 'middle_category_id' })
+  middleCategory: MiddleCategory | null
+
+  @Column({ type: 'int', nullable: false })
+  price: number
+
+  @Column({ type: 'tinyint', nullable: false, comment: '수입 || 지출' })
+  type: number
+
+  @Column({ type: 'date', name: 'registration_date', nullable: false, comment: '수입 지출내역 등록일' })
+  registrationDate: Date
+
+  @Column({ type: 'varchar', nullable: true })
+  memo: string | null
+
+  @CreateDateColumn({ type: 'date', name: 'created_date', nullable: false })
+  createdDate: Date
+
+  @UpdateDateColumn({ type: 'date', name: 'updated_date', nullable: true })
+  updatedDate: Date | null
+
+  @DeleteDateColumn({ type: 'date', name: 'deleted_date', nullable: true })
+  deletedDate: Date | null
+}
