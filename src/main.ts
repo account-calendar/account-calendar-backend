6edd569038a9807cafe83205e13335c983727fde
hjpark625 <zero950@gmail.com>
2025-06-15 20:15:50 +0900

    feat: 세션인증에 필요한 라이브러리 적용

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { NestFactory } from '@nestjs/core'
+import session from 'express-session'
+import passport from 'passport'
+import cookieParser from 'cookie-parser'
+
 import { AppModule } from '@/app.module'
 
 async function bootstrap() {
@@ -6,9 +10,27 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   console.log(`Server listening at port ${process.env.PORT}`)
 
+  app.use(cookieParser())
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'default_secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 1, // 1 hour
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // https only in production
+      },
+    }),
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
+
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   })
   await app.listen(PORT)
