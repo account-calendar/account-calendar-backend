7b52c34362b601944b8fb225b4668bfd149358ca
hjpark625 <zero950@gmail.com>
2025-06-15 20:16:29 +0900

    feat: 로그인시 세션정보 적용 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
 
 import { UserService } from '@/user/user.service'
 
-@Controller('user')
+import { LocalAuthGuard } from '@/user/auth.guard'
+import type { Request } from 'express'
+
+@Controller('auth')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('all')
-  findAllUsers() {
-    return this.userService.findAllUsers()
-  }
-
-  @Get(':id')
-  findUser(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.findUser(id)
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() { email, password }: { email: string; password: string }) {
+    return await this.userService.onLogin(email, password)
   }
 }
