ee8742219a1af2f2eb5b4a74e32adc703fd592b1
hjpark625 <zero950@gmail.com>
2025-06-15 20:16:52 +0900

    feat: 세션 인증 방식에 관련된 서비스 로직 수정 (로그인 기준만 제작)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,13 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { plainToInstance } from 'class-transformer'
+import bcrypt from 'bcrypt'
 
 import { User } from '@/entities/user.entity'
 import { UserDto } from '@/user/dto/user.dto'
@@ -14,21 +21,27 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async findAllUsers(): Promise<{ data: UserDto[] }> {
-    const allUsersInfo = await this.userRepository.find()
+  async onLogin(email: string, password: string) {
+    try {
+      const userData = await this.userRepository.findOne({ where: { email } })
 
-    const allUsersInfoDto = allUsersInfo.map((user) => plainToInstance(UserDto, user))
+      await this.validateUser(email, password)
 
-    if (allUsersInfoDto.length === 0) {
-      throw new HttpException('No users found', HttpStatus.NOT_FOUND)
-    }
+      const userInfo = plainToInstance(UserDto, userData)
 
-    return {
-      data: allUsersInfoDto,
+      return {
+        data: userInfo,
+      }
+    } catch (err: unknown) {
+      if (err instanceof HttpException) {
+        throw err
+      } else {
+        throw new InternalServerErrorException('로그인 중 오류가 발생했습니다. 다시 시도해주세요.')
+      }
     }
   }
 
-  async findUser(id: number): Promise<{ data: UserDto | null }> {
+  async findUser(id: number): Promise<{ data: UserDto }> {
     const foundUser = await this.userRepository.findOne({ where: { id } })
 
     if (foundUser === null) {
@@ -40,5 +53,20 @@ export class UserService {
     return {
       data: userInfo,
     }
+  }
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { email } })
+
+    if (!user) {
+      throw new BadRequestException('존재하지 않는 사용자입니다.')
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      throw new BadRequestException('이메일 또는 비밀번호가 일치하지 않습니다.')
+    }
+
+    return user
   }
 }
