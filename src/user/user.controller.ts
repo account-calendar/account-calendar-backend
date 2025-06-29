37553a0d5659b42014c7a13de4880af876a05cf1
hjpark625 <zero950@gmail.com>
2025-06-29 20:36:11 +0900

    feat: user 상세정보 컨트롤러 등록

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common'
 import { ApiBody, ApiOperation, ApiResponse, ApiTags, ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 import { UserService } from '@/user/user.service'
-import { LocalAuthGuard } from '@/user/auth.guard'
+import { AuthenticatedGuard, LocalAuthGuard } from '@/user/auth.guard'
+
 import type { Request } from 'express'
+import type { User } from '@/entities/user.entity'
 
 class LoginDto {
   @ApiProperty({
@@ -34,10 +36,23 @@ export class UserController {
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: '로그인', description: '이메일과 비밀번호로 로그인합니다.' })
   @ApiBody({ type: LoginDto })
-  @ApiResponse({ status: 200, description: '로그인 성공' })
-  @ApiResponse({ status: 400, description: '잘못된 요청' })
-  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiResponse({ status: HttpStatus.OK, description: '로그인 성공' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: '잘못된 요청' })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '인증 실패' })
   async login(@Body() { email, password }: LoginDto) {
     return await this.userService.onLogin(email, password)
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('user')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: '사용자 정보 조회', description: '로그인된 사용자의 정보를 세션을 통해 조회합니다.' })
+  @ApiResponse({ status: HttpStatus.OK, description: '사용자 정보 조회 성공' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: '사용자 정보가 없습니다.' })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '인증되지 않은 사용자' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: '세션 인증 실패 (만료 || 세션이 없는 사용자)' })
+  async getUser(@Req() req: Request & { user: User }) {
+    const user = req.user
+    return await this.userService.findUser(user)
   }
 }
