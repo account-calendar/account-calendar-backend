536acccf4a3456f58682b7f7cf19ad9baf9eef57
hjpark625 <zero950@gmail.com>
2025-07-07 21:28:02 +0900

    feat: 특정일자 별 수출입 내역 DTO 제작

--- src/transactions/dto/transactions.dto.ts
+++ src/transactions/dto/transactions.dto.ts
@@ -1,4 +1,8 @@
+import { Transform } from 'class-transformer'
 import { IsArray, IsObject } from 'class-validator'
+import { format } from 'date-fns'
+
+import type { Transaction } from '@/entities/transaction.entity'
 
 export class TransactionsDto {
   @IsObject()
@@ -6,4 +10,82 @@ export class TransactionsDto {
 
   @IsArray()
   dates: { [date: string]: { income: number; expense: number } }[]
+}
+
+export class TransactionsByDateDto {
+  @IsArray()
+  @Transform(({ value }) =>
+    value.map((item: Transaction) => ({
+      id: item.id,
+      price: item.price,
+      createdAt: format(item.createdDate, 'yyyy-MM-dd HH:mm:ss'),
+      updatedAt: item.updatedDate ? format(item.updatedDate, 'yyyy-MM-dd HH:mm:ss') : null,
+      deletedAt: item.deletedDate ? format(item.deletedDate, 'yyyy-MM-dd HH:mm:ss') : null,
+      registrationAt: format(item.registrationDate, 'yyyy-MM-dd HH:mm:ss'),
+      memo: item.memo,
+      majorCategory: item.majorCategory
+        ? {
+            id: item.majorCategory?.id || 0,
+            emoji: item.majorCategory?.emoji || null,
+            label: item.majorCategory?.label || '기타',
+          }
+        : null,
+      middleCategory: item.middleCategory
+        ? {
+            id: item.middleCategory?.id || 0,
+            emoji: item.middleCategory?.emoji || null,
+            label: item.middleCategory?.label || '기타',
+          }
+        : null,
+    })),
+  )
+  incomeList: {
+    id: number
+    price: number
+    memo: string | null
+    registrationAt: string
+    createdAt: string
+    updatedAt: string | null
+    deletedAt: string | null
+    majorCategory: { id: number; emoji: string | null; label: string } | null
+    middleCategory: { id: number; emoji: string | null; label: string } | null
+  }[]
+
+  @IsArray()
+  @Transform(({ value }) =>
+    value.map((item: Transaction) => ({
+      id: item.id,
+      price: item.price,
+      createdAt: format(item.createdDate, 'yyyy-MM-dd HH:mm:ss'),
+      updatedAt: item.updatedDate ? format(item.updatedDate, 'yyyy-MM-dd HH:mm:ss') : null,
+      deletedAt: item.deletedDate ? format(item.deletedDate, 'yyyy-MM-dd HH:mm:ss') : null,
+      registrationAt: format(item.registrationDate, 'yyyy-MM-dd HH:mm:ss'),
+      memo: item.memo,
+      majorCategory: item.majorCategory
+        ? {
+            id: item.majorCategory.id || 0,
+            emoji: item.majorCategory.emoji || null,
+            label: item.majorCategory.label || '기타',
+          }
+        : null,
+      middleCategory: item.middleCategory
+        ? {
+            id: item.middleCategory.id || 0,
+            emoji: item.middleCategory.emoji || null,
+            label: item.middleCategory.label || '기타',
+          }
+        : null,
+    })),
+  )
+  expenseList: {
+    id: number
+    price: number
+    memo: string | null
+    registrationAt: string
+    createdAt: string
+    updatedAt: string | null
+    deletedAt: string | null
+    majorCategory: { id: number; emoji: string | null; label: string } | null
+    middleCategory: { id: number; emoji: string | null; label: string } | null
+  }[]
 }
